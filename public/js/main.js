f00186cd10184a0674c5a735939d91dbddce03a4
Sofi <chaussettefurtive@outlook.fr>
2019-04-29 00:43:05 +0200

    Try to fix map images sizing

--- public/js/main.js
+++ public/js/main.js
@@ -5,26 +5,34 @@ $(function() {
 
     var timer = null;
 
-    function addAttr() {
+    function addAttr(test) {
+        console.log("i'm resized!");
         clearTimeout(timer);
-        timer = setTimeout(addSizeAttributes, 0);
+        timer = setTimeout(addSizeAttributes(test), 200);
     }
 
     
     $(".rwdimgmap").ready(function() {
-
+        var test = $(".rwdimgmap");
         if ($(this).width() != undefined) {
             // Add size attributes of the map image
-            addSizeAttributes();
-        }
-
+            addSizeAttributes(test);
+        }            
         // Detect when the window is resized
-        window.addEventListener('resize', addAttr, false);
+        window.addEventListener('resize', addAttr(test), true);
+    
     })
 
-    function addSizeAttributes() {
-        $(".rwdimgmap").attr("width", $(".rwdimgmap").width());
-        $(".rwdimgmap").attr("height", $(".rwdimgmap").height());
+    function addSizeAttributes(test) {        
+        var image = new Image();
+        image.src = test.attr("src");
+
+        var ratio = Math.max(test.width() / image.width, test.height() / image.height);
+        var width = Math.round(ratio * image.width);
+        var height = Math.round(ratio * image.height);
+        console.log("Taille retournée : " + width + " " + height);
+        test.attr("width", width);
+        test.attr("height", height);
     }
 
 });
